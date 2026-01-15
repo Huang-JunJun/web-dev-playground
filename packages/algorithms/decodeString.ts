b02996a0a75750eecf186590bd460c1abd0ccfbb
huangjunjun <770894595@qq.com>
2026-01-15 12:42:31 +0800

    feat(algorithms): add decode string solution

--- packages/algorithms/decodeString.ts
+++ packages/algorithms/decodeString.ts
@@ -6,15 +6,15 @@ export const decodeString = (s: string): string => {
   let k = 0
 
   for (const ch of s) {
-    if(ch >= '0' && ch <= '9') {
-      k = k * 10 + parseInt(ch)
+    if (ch >= '0' && ch <= '9') {
+      k = k * 10 + parseInt(ch, 10)
     } else if (ch === '[') {
-      stack.push({ prev: currentString, k: ch.charCodeAt(0) - 48 })
+      stack.push({ prev: currentString, k })
       currentString = ''
       k = 0
     } else if (ch === ']') {
       const top = stack.pop()!
-      currentString = top.prev +currentString.repeat(k)
+      currentString = top.prev + currentString.repeat(top.k)
     } else {
       currentString += ch
     }
